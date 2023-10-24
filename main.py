a472cfe6c98c53292f74bdb68e0e547a28e98055
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-24 14:28:32 +0200

    Added apps' names, images, and descriptions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout='wide', page_title="Portfolio")
 
 col1, col2 = st.columns(2)
 
@@ -14,3 +15,24 @@ with col2:
      to the knee and I'm a Python programmer now :)
     """
     st.info(bio)
+
+content = """Below you can find some of the apps I have built in Python. Feel free to contact me."""
+st.text(content)
+
+col3, col4 = st.columns(2)
+
+data = pd.read_csv('data.csv', sep=';')
+
+
+def display_project():
+    st.header(r['title'])
+    st.image(image=f"images/{r['image']}")
+    st.write(r['description'])
+
+
+with col3:
+    for i, r in data[0::2].iterrows():
+        display_project()
+with col4:
+    for i, r in data[1::2].iterrows():
+        display_project()
