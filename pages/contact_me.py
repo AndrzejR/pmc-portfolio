9f39eea07dfb10cfefc6f908b0b7a6edff48e9d1
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-25 07:26:08 +0200

    Added contact form

--- pages/contact_me.py
+++ pages/contact_me.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.header("Contact me")
+st.title("Contact Me")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Your email address")
+    user_message = st.text_area("Your message")
+    submit = st.form_submit_button("Submit")
+    if submit:
+        st.info("Submitted!")
