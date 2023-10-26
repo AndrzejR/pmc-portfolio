5dc470d09b050d3abe023808f6e2e4c0e161cd1f
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-10-26 07:23:05 +0200

    Added actually sending emails to the Contact form

--- functions/send_email.py
+++ functions/send_email.py
@@ -1,16 +1,22 @@
 import os, smtplib, ssl
 
-host = 'smtp.gmail.com'
-port = 465
-username = 'developmentandrzejr@gmail.com'
-password = os.environ['PMCGooglePass']
-context = ssl.create_default_context()
-receiver = 'developmentandrzejr@gmail.com'
 
-message = """Subject: Hello from Python!
-Hello! This is an email sent from Python.
-"""
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    username = 'developmentandrzejr@gmail.com'
+    password = os.environ['PMCGooglePass']
+    context = ssl.create_default_context()
+    receiver = 'developmentandrzejr@gmail.com'
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+if __name__ == "__main__":
+    message = """Subject: Test Hello from send_email
+    send_email was run as __main__
+    """
+    send_email(message)
--- pages/contact_me.py
+++ pages/contact_me.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import functions.send_email as se
 
 st.title("Contact Me")
 
 with st.form(key="contact_form"):
     user_email = st.text_input("Your email address")
     user_message = st.text_area("Your message")
+    message = f"""Subject: Portfolio Message from {user_email}
+
+From: {user_email}
+{user_message}
+    """
     submit = st.form_submit_button("Submit")
     if submit:
+        se.send_email(message)
         st.info("Submitted!")
